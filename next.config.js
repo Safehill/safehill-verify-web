192964a2408448732f4d3e43d0106963e6822728
gennaro <gennaro@safehill.io>
2025-12-17 11:30:44 -0800

    chore: disable turbopack

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    turbo: false,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
